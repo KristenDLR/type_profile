00485c926e8177de17810ab5530f0e7e0fa9683f
Kristen De La Rosa <delarosa.kristen@gmail.com>
2024-11-14 18:00:11 -0800

    feat/about section created, google fonts wonky behavior, updated styles pallet

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,6 +1,9 @@
 import logo from "./logo.png";
 import rose from "./rose.png";
+import greenMe from "./greenMe.png";
 
 export const logoImg = logo;
 
 export const roseImg = rose;
+
+export const greenMeImg = greenMe;
---
+++ src/theme/theme.ts
@@ -0,0 +1,16 @@
+import { createTheme } from "@mui/material";
+import "./theme.css";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "##ff8400",
+    },
+    success: {
+      main: "#95c289",
+    },
+    secondary: {
+      main: "#efc918",
+    },
+  },
+});
